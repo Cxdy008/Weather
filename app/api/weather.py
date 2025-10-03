bf03bb892f2ca1dd351afb701f16198ff1309bde
dizonovundu <dizonovundu@gmail.com>
2025-10-04 00:04:33 +0100

    post mock added

--- app/api/weather.py
+++ app/api/weather.py
@@ -4,6 +4,11 @@ from pydantic import BaseModel
 router = APIRouter()
 
 
+class LocationRequest(BaseModel):
+    lat: float
+    lng: float
+
+
 # uma rota só para testar a funcionamento
 @router.get("/")
 async def root():
@@ -15,6 +20,31 @@ def mock_forecast():
     return {
         "lat": -8.839988,
         "lng": 13.289436,
+        "local": "Luanda",
+        "pais": "Angola",
+        "data": "2024-06-26",
+        "dados": {
+            "medTemp": 24,
+            "medChuva": 0,
+            "medVelocidade": 10,
+            "all": [
+                {"hora": "00", "temp": 24, "chuva": 0, "velocidade": 10},
+                {"hora": "01", "temp": 24, "chuva": 0, "velocidade": 10},
+                {"hora": "02", "temp": 24, "chuva": 0, "velocidade": 10},
+                {"hora": "03", "temp": 24, "chuva": 0, "velocidade": 20},
+                {"hora": "04", "temp": 24, "chuva": 0, "velocidade": 6},
+                # ... pode completar até 23
+                {"hora": "23", "temp": 24, "chuva": 0, "velocidade": 10},
+            ],
+        },
+    }
+
+
+@router.post("/forecast")
+def forecast(location: LocationRequest):
+    return {
+        "lat": location.lat,
+        "lng": location.lng,
         "local": "Luanda",
         "pais": "Angola",
         "data": "2024-06-26",

179cdbd3c402bbb25aa2ad8470ded1fe6eb726c2
dizonovundu <dizonovundu@gmail.com>
2025-10-05 14:17:42 +0100

    CORS handled

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router as api_router
 
 app = FastAPI(
     title="Weather App",
     description="API para prever para previsões climáticas usando dados históricos da NASA e modelos de machine learning",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # registro de todas as rotas da API
